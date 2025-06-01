a34f695377b3eb4028e3ade23f9fe1a31103548d
psjw <dlrudtn108@gmail.com>
2025-06-01 13:05:14 +0900

    first

---
+++ docker-springboot/src/main/java/com/psjw/dockerspringboot/AppController.java
@@ -0,0 +1,14 @@
+package com.psjw.dockerspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+
+}
